6f91625026cd3987dc90be5773f3c334c1c25d4b
Jonas <62521337+jonasyr@users.noreply.github.com>
2024-03-06 19:09:05 +0100

    Update ProgressDisplayScript.ps1

--- ProgressDisplayScript.ps1
+++ ProgressDisplayScript.ps1
@@ -7,12 +7,20 @@ while ($true) {
         try {
             $progress = Get-Content $ProgressFilePath -ErrorAction Stop
             Write-Progress -PercentComplete $progress -Status "Processing" -Activity "Deleting Empty Folders"
+            
+            # Check if progress is 100 and exit if true
+            if ($progress -eq 100) {
+                Write-Host "Task completed. Exiting..."
+                Start-Sleep -Seconds 2  # Small delay to ensure the user sees the completion message
+                exit
+            }
+
         } catch {
             Write-Host "Waiting for progress update..."
         }
     } else {
         Write-Host "Progress file not found. Exiting..."
-        break
+        exit
     }
     Start-Sleep -Seconds 1
 }
